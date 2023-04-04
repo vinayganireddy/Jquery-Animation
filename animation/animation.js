8cc4ad78d5a7f27b70129374ef087d53c2601b32
vinayganireddy <122338780+vinayganireddy@users.noreply.github.com>
2023-04-04 11:43:28 +0530

    Add files via upload

---
+++ animation/animation.js
@@ -0,0 +1,96 @@
+// $(document).ready(()=>
+// {
+//     $("button").click(()=>
+//     {
+//         $("div").animate({left:"1400px"});
+//     })
+// })
+
+
+// $(document).ready(()=>
+// {
+//     $("button").click(()=>
+//     {
+//         $("div").animate({
+//             left:"1000px",
+//             height:'+=150px',
+//             width:'+=250px'
+//         });
+//     })
+// })
+
+// $(document).ready(()=>
+// {
+//     $("button").click(()=>
+//     {
+//         $("div").animate({
+//             left:"100px",
+//             bottom:'110px',
+//             right:'250px',
+//             top:'200px'
+//         });
+//     })
+// })
+
+// $(document).ready(()=>
+// {
+//     $("button").click(()=>
+//     {
+//         $("div").animate({left:"1000px"});
+//     })
+//     $("button").click(()=>
+//     {
+//         $("div").animate({bottom:"0px"});
+//     })
+//     $("button").click(()=>
+//     {
+//         $("div").animate({left:"0px"});
+//     })
+//     $("button").click(()=>
+//     {
+//         $("div").animate({top:"0px"});
+//     })
+// })
+
+
+$(document).ready(()=>
+{
+    $("button").click(()=>
+    {
+        $("#div1").animate({left:"50%"})
+        $("#div1").animate({top:"50%"});
+    })
+})
+$(document).ready(()=>
+{
+    $("button").click(()=>
+    {   
+        $("#div2").animate({left:"0%"})
+        $("#div2").animate({top:"85%"})
+        $("#div2").animate({right:"0%"})
+        $("#div2").animate({top:"0%"})
+        $("#div2").animate({left:"50%"})
+        $("#div2").animate({top:"50%"});
+        
+    })
+})
+$(document).ready(()=>
+{
+    $("button").click(()=>
+    {    $("#div3").animate({left:"0"})
+         $("#div3").animate({top:"0%"})
+         $("#div3").animate({right:"50%"})
+        $("#div3").animate({left:"50%"})
+        $("#div3").animate({top:"50%"});
+       
+    })
+})
+$(document).ready(()=>
+{
+    $("button").click(()=>
+    {
+        $("#div4").animate({left:"50%"})
+        $("#div4").animate({top:"50%"});
+      
+    })
+})
